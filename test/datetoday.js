df3185745cd12be53965783b3eebd94aed02ddf2
bloodyKnuckles <treenopie@gmail.com>
2015-11-27 14:21:32 -0600

    updated dates object

---
+++ test/datetoday.js
@@ -0,0 +1,18 @@
+var datesobj = require('../lib/datesobj')
+var test = require('tape')
+
+var dd = new Date(),
+    getfullyear = dd.getFullYear(),
+    getmonth = String('00' + (dd.getMonth() + 1)).slice(-2),
+    getdate = String('00' + dd.getDate()).slice(-2),
+
+    todaydate = getfullyear + '-' + getmonth + '-' + getdate
+    '-' + String('00' + (dd.getMonth() + 1)).slice(-2) +
+    '-' + String('00' + dd.getDate()).slice(-2),
+    nowdate = todaydate + ' ' + dd.toTimeString().split(' ')[0]
+
+test('todaydate', function (tt) {
+    tt.plan(1)
+    tt.equal(datesobj.todaydate, todaydate)
+})
+
