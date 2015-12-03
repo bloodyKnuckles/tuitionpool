25be347263141e306a04c3f7aa68fb5274dc52ba
bloodyKnuckles <treenopie@gmail.com>
2015-12-03 10:49:35 -0600

    updated db schema

--- db/schema2.sql
+++ db/schema2.sql
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `tuitionpool`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `tuitionpool` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `tuitionpool`;
+
+--
 -- Table structure for table `pools`
 --
 
@@ -23,12 +31,13 @@ DROP TABLE IF EXISTS `pools`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pools` (
-  `pool` binary(16) NOT NULL,
+  `pooltoken` binary(16) NOT NULL,
   `poolname` text,
   `datetime_start` datetime DEFAULT NULL,
   `datetime_end` datetime DEFAULT NULL,
-  UNIQUE KEY `pool` (`pool`),
-  UNIQUE KEY `pool_2` (`pool`)
+  PRIMARY KEY (`pooltoken`),
+  UNIQUE KEY `pool` (`pooltoken`),
+  UNIQUE KEY `pool_2` (`pooltoken`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -40,25 +49,23 @@ DROP TABLE IF EXISTS `requests`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `requests` (
-  `pool` binary(16) NOT NULL,
+  `pooltoken` binary(16) DEFAULT NULL,
   `email` varchar(64) DEFAULT NULL,
   `name` varchar(64) DEFAULT NULL,
-  `provisional` tinyint(1) DEFAULT NULL,
+  `provisional` tinyint(1) DEFAULT '0',
   `coursenumber` varchar(32) DEFAULT NULL,
   `coursename` varchar(64) DEFAULT NULL,
   `universityname` varchar(64) DEFAULT NULL,
   `coursetype` varchar(16) DEFAULT NULL,
   `coursedegree` tinyint(1) DEFAULT NULL,
   `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  KEY `FK_requests` (`pool`),
-  CONSTRAINT `FK_requests` FOREIGN KEY (`pool`) REFERENCES `pools` (`pool`) ON DELETE CASCADE ON UPDATE CASCADE
+  `dateonly` varchar(11) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT '1',
+  UNIQUE KEY `UQ_requests` (`pooltoken`,`email`,`dateonly`),
+  KEY `FK_requests` (`pooltoken`),
+  CONSTRAINT `FK_pooltoken` FOREIGN KEY (`pooltoken`) REFERENCES `pools` (`pooltoken`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping routines for database 'tuitionpool'
---
-/*!50003 DROP FUNCTION IF EXISTS `ordered_uuid` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -68,9 +75,13 @@ CREATE TABLE `requests` (
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50020 DEFINER=`root`@`localhost`*/ /*!50003 FUNCTION `ordered_uuid`(uuid BINARY(36)) RETURNS binary(16)
-    DETERMINISTIC
-RETURN UNHEX(CONCAT(SUBSTR(uuid, 15, 4),SUBSTR(uuid, 10, 4),SUBSTR(uuid, 1, 8),SUBSTR(uuid, 20, 4),SUBSTR(uuid, 25))) */;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER tuitionpool_requests_date_trigger
+    BEFORE INSERT
+    ON tuitionpool.requests
+    FOR EACH ROW
+BEGIN
+    SET NEW.dateonly = LEFT(NEW.datetime, 11);
+END */;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -86,4 +97,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-17 10:04:25
+-- Dump completed on 2015-12-03 10:48:56
