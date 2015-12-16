c0a50d2142a613b1524ebe1926e1396e984eea5e
bloodyKnuckles <treenopie@gmail.com>
2015-12-16 12:05:53 -0600

    added images directory

--- bin/cmd.js
+++ bin/cmd.js
@@ -17,42 +17,46 @@ if (secargv._[0] === 'init') {
   //return
 }
 
-var alloc = require('tcp-bind')
-var secfd = alloc(secargv.port)
+else { // alternative to return
 
-if (secargv.gid) process.setuid(secargv.gid)
-if (secargv.uid) process.setuid(secargv.uid)
+    var alloc = require('tcp-bind')
+    var secfd = alloc(secargv.port)
 
-var https = require('https')
-var createApp = require('../')
-var app = createApp()
-var secserver = https.createServer({
-    key : fs.readFileSync(serverconn.privkey),
-    cert: fs.readFileSync(serverconn.cert)
-  },
-  function (req, res) {
-    app.handle(req, res)
-})
-secserver.listen({ fd: secfd }, function () {
-  console.log('listening on :' + secserver.address().port)
-})
+    if (secargv.gid) process.setuid(secargv.gid)
+    if (secargv.uid) process.setuid(secargv.uid)
 
-var wsock = require('websocket-stream')
-wsock.createServer({ server: secserver }, function (stream) {
-  stream.pipe(app.createStream()).pipe(stream)
-})
+    var https = require('https')
+    var createApp = require('../')
+    var app = createApp()
+    var secserver = https.createServer({
+        key : fs.readFileSync(serverconn.privkey),
+        cert: fs.readFileSync(serverconn.cert)
+      },
+      function (req, res) {
+        app.handle(req, res)
+    })
+    secserver.listen({ fd: secfd }, function () {
+      console.log('listening on :' + secserver.address().port)
+    })
 
-var argv = minimist(process.argv.slice(2), {
-  alias: { p: 'port', u: 'uid', g: 'gid' },
-  default: { port: isroot() ? 80 : 8000 }
-})
-var fd = alloc(argv.port)
-var http = require('http')
-var server = http.createServer(function (req, res) {
-  res.writeHead(301, { 'Location': serverconn.redirhttps + req.url });
-  res.end();
-})
-server.listen({ fd: fd }, function () {
-  console.log('listening on :' + server.address().port)
-})
+    var wsock = require('websocket-stream')
+    wsock.createServer({ server: secserver }, function (stream) {
+      stream.pipe(app.createStream()).pipe(stream)
+    })
+
+    var argv = minimist(process.argv.slice(2), {
+      alias: { p: 'port', u: 'uid', g: 'gid' },
+      default: { port: isroot() ? 80 : 8000 }
+    })
+    var fd = alloc(argv.port)
+    var http = require('http')
+    var server = http.createServer(function (req, res) {
+      res.writeHead(301, { 'Location': serverconn.redirhttps + req.url });
+      res.end();
+    })
+    server.listen({ fd: fd }, function () {
+      console.log('listening on :' + server.address().port)
+    })
+
+}
 
